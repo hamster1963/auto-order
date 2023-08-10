4c3ff402e75f1c4d95ada67c24ad445920a0a6ad
hamster1963 <1410514192@qq.com>
2023-08-10 11:01:11 +0800

    refactor: 将信息存储结构改为结构体

--- main.go
+++ main.go
@@ -5,7 +5,6 @@ import (
 	"bufio"
 	"context"
 	"fmt"
-	"github.com/gogf/gf/v2/frame/g"
 	"github.com/gogf/gf/v2/os/glog"
 	"github.com/gogf/gf/v2/os/gtime"
 	"math/rand"
@@ -22,7 +21,7 @@ func main() {
 	glog.Info(context.Background(), "自动决定中午吃什么机器人v0.1启动成功！")
 	glog.Info(context.Background(), "自动决定中午吃什么机器人v0.1开始工作！")
 	glog.Info(context.Background(), "正在决定中......")
-	time.Sleep(3 * time.Second)
+	time.Sleep(1 * time.Second)
 	for {
 		reader := bufio.NewReader(os.Stdin)
 		glog.Info(context.Background(), "按下回车键进行显示：")
@@ -30,44 +29,40 @@ func main() {
 		if err != nil {
 			fmt.Println("读取输入时发生错误:", err)
 		}
-		glog.Info(context.Background(), "正在选择......")
 		glog.Info(context.Background(), "正在决定中......")
-		orderMap := GetRandomOrder()
-		glog.Warning(context.Background(), "今天中午吃：", orderMap["key"], " 的 ", orderMap["value"])
-		time.Sleep(3 * time.Second)
+		orderData := GetRandomOrder()
+		glog.Warning(context.Background(), "今天中午吃：", orderData.Canteen, " 的 ", orderData.Order)
+		time.Sleep(2 * time.Second)
 	}
 }
 
-func GetRandomOrder() g.Map {
+// TodayOrder 今日餐厅与菜品
+type TodayOrder struct {
+	Canteen string
+	Order   string
+}
+
+// GetRandomOrder 获取随机餐厅与菜品
+func GetRandomOrder() *TodayOrder {
 	// 将 Map 的键存储到切片中
 	keys := make([]string, 0, len(data.CanteenOrder))
 	for k := range data.CanteenOrder {
 		keys = append(keys, k)
 	}
-
-	// 设置随机种子
-	rand.Seed(time.Now().UnixNano())
-
 	// 生成随机索引
 	randomIndex := rand.Intn(len(keys))
-
 	// 获取随机键
 	randomKey := keys[randomIndex]
-
-	fmt.Println("随机获取的键:", randomKey)
 	// 随机从map中获取一个key
 	list := data.CanteenOrder[randomKey]
-	// 设置随机种子
-	rand.Seed(time.Now().UnixNano())
 
 	// 生成随机索引
 	randomIndex = rand.Intn(len(list))
-
-	// 获取随机值
+	// 获取菜品随机值
 	randomValue := list[randomIndex]
 
-	return g.Map{
-		"key":   randomKey,
-		"value": randomValue,
+	return &TodayOrder{
+		Canteen: randomKey,
+		Order:   randomValue,
 	}
 }
